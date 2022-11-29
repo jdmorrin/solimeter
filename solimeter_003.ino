60633ed3f5a464df3b30529b50c4c833226012bb
Juandi <morrincomero@outlook.com>
2022-11-29 12:40:00 -0500

    Solimeter basic

---
+++ solimeter_003.ino
@@ -0,0 +1,47 @@
+/*
+  Solimeter project: 
+*/
+
+// analog pins
+const int analogIn = A0;
+const int analogOut = 9;
+
+int input;
+long startTime = 0;
+long elapsedTime = 0;
+long totalTime = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  input = analogRead(analogIn);
+  Serial.print(input); Serial.print("\t");
+
+  if(input >= 850){
+    if(startTime == 0){
+      startTime = millis();
+    }else{
+      elapsedTime = (millis() - startTime)/1000.0;
+      Serial.print(elapsedTime);
+    }
+    delay(1000);   
+  }else if(input < 850){
+    totalTime = totalTime + elapsedTime;
+    elapsedTime = 0;
+    startTime = 0;
+    Serial.print("Total sunlight time: "); Serial.print(totalTime);
+  }
+
+  Serial.println();
+  
+  if(input == 0){
+    Serial.print("The total time today was: ");
+    Serial.print(totalTime);
+    Serial.println();
+    Serial.end();
+  }
+}
