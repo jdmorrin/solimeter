dad0f560b97370bd267eee8e40355d0be7dd6b99
Juandi <morrincomero@outlook.com>
2022-11-29 21:29:38 -0500

    Added LCD

--- solimeter_003.ino
+++ solimeter_003.ino
@@ -3,11 +3,32 @@
 
   Circuit: connect + end of of solar cell to pin "A0"
            connect - end to GND pin
+
+  LCD:  Vss => GND
+        Vdd => +
+        Vo => Potentiometer mid pin
+        RS => 12
+        RW => GND
+        E => 11
+        K => GND
+        A => 330 Ohms => +
+        D7 => 2
+        D6 => 3
+        D5 => 4
+        D4 => 5        
+        
 */
 
-// analog pins
+#include <LiquidCrystal.h>
+
+
+// pins
 const int analogIn = A0;
 const int analogOut = 9;
+const int rs = 12, e =11, d7= 2, d6 = 3, d5 = 4, d4 = 5; 
+
+// LCD
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
 
 int input;
 long startTime = 0;
@@ -15,13 +36,15 @@ long elapsedTime = 0;
 long totalTime = 0;
 
 void setup() {
-  
+  lcd.begin(16,1);
+//  lcd.print("Hello World!");
   Serial.begin(9600);
 }
 
 void loop() {
   input = analogRead(analogIn);
   Serial.print(input); Serial.print("\t");
+  lcd.print(input); lcd.print("\n");
 
   // 
   if(input >= 850){
